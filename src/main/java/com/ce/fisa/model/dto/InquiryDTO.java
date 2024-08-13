602081ea28c30a38e4b122a99964202cbd22e7ce
LeeYeonhee-00 <dldusgml0929@gachon.ac.kr>
2024-08-13 15:52:50 +0900

    i################header feat : 전체 Inquiry 리스트 조회 기능 추가

--- src/main/java/com/ce/fisa/model/dto/InquiryDTO.java
+++ src/main/java/com/ce/fisa/model/dto/InquiryDTO.java
@@ -2,40 +2,24 @@ package com.ce.fisa.model.dto;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Table(name = "Inquiry")
-@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class InquiryDTO {
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
-	@Column(name = "inquiry_id")
+	
 	private long inquiry_id;
-
-	@ManyToOne
-	@JoinColumn(name = "user_id")
 	private UserDTO user_id;
-
-	@ManyToOne
-	@JoinColumn(name = "work_id")
 	private WorkDTO work_id;
-
-	@Column(name = "inquiry_date")
 	private LocalDateTime inquiry_date;
-
-	@Column(name = "inquiry_address")
 	private String inquiry_address;
-
-	@Column(name = "inquiry_content")
 	private String inquiry_content;
-
-	@Column(name = "inquiry_title")
 	private String inquiry_title;
 }
