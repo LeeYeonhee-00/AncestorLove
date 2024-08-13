6d88791d2af99da622d36fdc16ffd00df6e4d5f3
isshomin <zx149@naver.com>
2024-08-13 11:44:30 +0900

    chore: partner 이미지 컬럼 추가

--- src/main/java/com/ce/fisa/controller/PartnerController.java
+++ src/main/java/com/ce/fisa/controller/PartnerController.java
@@ -1,8 +1,9 @@
 package com.ce.fisa.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class PartnerController {
 
+	
 }
