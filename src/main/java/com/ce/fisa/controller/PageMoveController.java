3a250f0d76da5d373cc5c57c53a467ccd86b604d
castlhoo <ksungho9991@gmail.com>
2024-08-15 02:50:43 +0900

    design : write.html 수정 및 DB연결
    필요 의존성 설정
    데이터베이스 연결 설정
    기본 디렉토리 구조 구축

--- src/main/java/com/ce/fisa/controller/PageMoveController.java
+++ src/main/java/com/ce/fisa/controller/PageMoveController.java
@@ -1,7 +1,10 @@
 package com.ce.fisa.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageMoveController {
@@ -10,5 +13,5 @@ public class PageMoveController {
 	public String home() {
 		return "main";
 	}
-	
+
 }
