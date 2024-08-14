1e851c8f9e7b005da35137f61cf1771b3017c5b6
ohwoong2 <ohwoonge@gmail.com>
2024-08-14 11:20:17 +0900

    feat : 유저 로그인/회원가입 예외 추가, 로그인 로직 변경
    로그인 시 필요한 값 유저 네임에서 이메일로 변경

--- src/main/java/com/ce/fisa/dao/UserRepository.java
+++ src/main/java/com/ce/fisa/dao/UserRepository.java
@@ -3,6 +3,7 @@ package com.ce.fisa.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.ce.fisa.model.dto.UserDTO;
 import com.ce.fisa.model.entity.User;
 import java.util.List;
 
@@ -11,5 +12,7 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 	User findByUserName(String userName);
+
+	User findByUserEmail(String userEmail);
 
 }
--- src/main/java/com/ce/fisa/service/UserServiceImpl.java
+++ src/main/java/com/ce/fisa/service/UserServiceImpl.java
@@ -1,50 +1,67 @@
 package com.ce.fisa.service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ce.fisa.controller.UserController;
 import com.ce.fisa.dao.UserRepository;
+import com.ce.fisa.exception.InvalidSignupException;
+import com.ce.fisa.exception.NotExistInquiryException;
+import com.ce.fisa.exception.NotExistUserException;
+import com.ce.fisa.model.dto.UserDTO;
 import com.ce.fisa.model.entity.User;
 
 import jakarta.servlet.http.HttpSession;
 
 @Service
 public class UserServiceImpl implements UserService {
-	
+	private static final Logger logger = LogManager.getLogger(UserController.class);
+
+
 	@Autowired
 	private UserRepository userRepository;
 
 	@Autowired
-    private HttpSession httpSession; // 세션 객체 주입
-	
+	private HttpSession httpSession; // 세션 객체 주입
+	// DTO <-> Entity
+	private ModelMapper mapper = new ModelMapper();
+
 	@Override
-	public boolean signupUser(User user) {
+	public boolean signupUser(UserDTO userDTO) throws InvalidSignupException {
+		// UserDTO -> User
+		 if (userDTO.getUserName() == null || userDTO.getUserName().isEmpty() ||
+		            userDTO.getUserEmail() == null || userDTO.getUserEmail().isEmpty() ||
+		            userDTO.getUserPw() == null || userDTO.getUserPw().isEmpty()) {
+		            throw new InvalidSignupException("사용자 이름, 이메일 또는 비밀번호가 누락되었습니다.");
+		        }
+		User user = mapper.map(userDTO, User.class);
 		User result = userRepository.save(user);
-		if(result == null) {
-			// 추후 추가
-		}
+		
 		return true;
 	}
-	public boolean authenticate(String userName, String userPW) {
-        User user = userRepository.findByUserName(userName);
-        if (user != null) {
 
-        	
-        	
-        	if (userPW.equals(user.getUserPw())) {
-                // 로그인 성공 시 세션에 사용자 정보 저장
-                httpSession.setAttribute("user", user);
-				
+	public boolean authenticate(String userEmail, String userPw) throws NotExistUserException {
+		User user = userRepository.findByUserEmail(userEmail);
+		if (user != null) {
+
+			if (userPw.equals(user.getUserPw())) {
+				// 로그인 성공 시 세션에 사용자 정보 저장
+				httpSession.setAttribute("userId", user.getUserId());
+				httpSession.setAttribute("userName", user.getUserName());
+
+
 //				  세션 사용법 
-				  System.out.println (httpSession.getAttribute("user")); User user2 =
-				  (User) httpSession.getAttribute("user");
-				  System.out.println(user2.getUserName());
-				 
-                return true;
-            }
+				logger.debug("사용자 id: " + httpSession.getAttribute("userId"));
+				logger.debug("사용자 이름: " +httpSession.getAttribute("userName"));
 
-        }
-        return false;
-    }
+				return true;
+			}
+
+		}
+		throw new NotExistUserException("해당 유저는 존재하지 않습니다.");
+	}
 
 }
